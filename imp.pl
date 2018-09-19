cfe95c9a0c62b60aa75e1a377934998ae2b57815
Erik Örjehag <erior950@student.liu.se>
2018-09-19 11:49:39 +0200

    Working replace and evaluate can handle multiple elements in it's binding environment

--- imp.pl
+++ imp.pl
@@ -1,17 +1,35 @@
 
 % Helpers
-replace(_, _, [], []).
-replace(N, X, [X=Z|S], [X=N|Sn]):-
-  replace(N, X, S, Sn).
-replace(N, X, [S0|S], [S0|Sn]) :-
-  replace(N, X, S, Sn).
+exists([], _).
+exists(X, [X=_|Xs]) :-
+  exists([], Xs).
+exists(X, [Y=_|Ys]) :-
+  dif(X, Y),
+  exists(X, Ys).
+
+del(X, [X=_|T], T).
+del(X, [Y|T], [Y|R]) :-
+    del(X, T, R).
+
+app(Ys, [], Ys).
+app(Ys, [X|Xs], [X|Zs]) :-
+    app(Ys, Xs, Zs).
+
+replace(X, N, S0, Sn) :-
+  del(X, S0, S1),
+  app([X=N], S1, Sn).
+replace(X, N, S0, Sn) :-
+  \+ exists(X, S0),
+  app([X=N], S0, Sn).
 
 % Commands
 execute(S0, skip, S0) :- nl, write('skip'), nl, write(S0), nl.
 
 execute(S, set(X, E), Sn) :-
+  nl, write('set '), write(X), write('='), write(E), nl, write(S), nl,
   evaluate(S, E, N),
-  replace(N, X, S, Sn).
+  nl, write(S), write(' '), write(E), write(' '), write(N), nl,
+  replace(X, N, S, Sn).
 
 execute(S0, if(B, C1, _), Sn) :-
   evaluate(S0, B, tt),
@@ -32,8 +50,16 @@ execute(S0, seq(C1, C2), S2) :-
   nl, write(S2), nl.
 
 % Expressions
-evaluate(_, num(N), N).
+evaluate(_, [], _).
 evaluate([I=N], id(I), N).
+evaluate([I=N|Is], id(I), N) :-
+  dif(Is,[]),
+  evaluate(Is, [], N).
+evaluate([X=_|Is], id(I), N) :-
+  dif(X, I),
+  evaluate(Is, id(I), N).
+
+evaluate(_, num(N), N).
 evaluate(S, E1 + E2, N3) :-
   evaluate(S, E1, N1),
   evaluate(S, E2, N2),
@@ -72,6 +98,7 @@ num(N) :- [N], { number(N) }.
 
 /* execute([a=3], set(x, id(a)), Sn). */
 /* execute([], set(x, num(5)), Sn). */
+/* execute([a=1, x=3, b=2], set(x, num(5)), Sn). */
 /* evaluate([x=3], id(x), N). */
 /* evaluate([x=3], id(x) + num(5), N). */
 /* evaluate([x=3], id(x) / num(5), N). */
@@ -84,6 +111,11 @@ num(N) :- [N], { number(N) }.
 /* execute([x=4], if(id(x) > num(3), set(x, num(0)), set(x, num(1))), Sn). */
 
 /*
+
+evaluate([x=4,y=1],id(x)-num(1), N).
+evaluate([x=4],id(x)-num(1), N).
+evaluate([x=4,y=1], id(x), N).
+
 evaluate([x=3], id(x) > num(1), tt).
 execute([x=3], set(x, id(x) - num(1)), Sn).
 
@@ -131,7 +163,7 @@ execute([x=100],
   ), Sn
 ).
 
-execute([x=100],
+execute([x=10],
   while(
     id(x) > num(1),
     seq(
