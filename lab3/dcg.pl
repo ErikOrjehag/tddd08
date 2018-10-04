4711031e0d6758188ec4ab0ebfc92d6e97b5b577
Erik Örjehag <erior950@student.liu.se>
2018-10-04 12:52:14 +0200

    Added one test case

--- lab3/dcg.pl
+++ lab3/dcg.pl
@@ -29,6 +29,9 @@ pgm(L, Ln, seq(Cmd, Pgm)) :-
   consume(;, L1, L2),
   pgm(L2, Ln, Pgm).
 
+/*
+predicates for handling commands
+*/
 cmd([skip|L], L, skip).
 cmd([id(X), :=|L], Ln, set(X, Expr)) :-
   expr(L, Ln, Expr).
@@ -45,6 +48,9 @@ cmd([while|L], Ln, while(Bool, Pgm)) :-
   pgm(L2, L3, Pgm),
   consume(od, L3, Ln).
 
+/*
+predicates for handling boolean expressions
+*/
 bool(L, Ln, Expr1 > Expr2) :-
   expr(L, L1, Expr1),
   consume(>, L1, L2),
@@ -54,6 +60,9 @@ bool(L, Ln, Expr1 < Expr2) :-
   consume(<, L1, L2),
   expr(L2, Ln, Expr2).
 
+/*
+predicates for handling expressions
+*/
 expr(L, Ln, Factor * Expr) :-
   factor(L, L1, Factor),
   consume(*, L1, L2),
@@ -61,6 +70,9 @@ expr(L, Ln, Factor * Expr) :-
 expr(L, Ln, Factor) :-
   factor(L, Ln, Factor).
 
+/*
+predicates for handling factors
+*/
 factor(L, Ln, Term + Factor) :-
   term(L, L1, Term),
   consume(+, L1, L2),
@@ -68,6 +80,9 @@ factor(L, Ln, Term + Factor) :-
 factor(L, Ln, Term) :-
   term(L, Ln, Term).
 
+/*
+predicates for handling terms
+*/
 term([id(X)|Ln], Ln, id(X)).
 term([num(X)|Ln], Ln, num(X)).
 
@@ -76,13 +91,20 @@ term([num(X)|Ln], Ln, num(X)).
 
 TEST CASE:
 
-run(
+| ?- run(
   [x=3],
-  "y:=1; z:=0; while x>z do z:=z+1; y:=y*z od",
+  "y:=3; z:=0; while x>z do z:=z+1; y:=y*z od",
   Res
 ).
-
 Res = [x=3,z=3,y=6] ?;
+no
+
+| ?- run(
+  [x=3],
+  "y:=1; z:=0; while x>z do while x+1>y do y:=y+1 od; z:=z+1; y:=y*z od",
+  Res
+).
+Res = [x=3,z=3,y=24] ? ;
 no
 
 */
