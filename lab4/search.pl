e876c12190c175f35de835adace527ef3a8cbb63
Erik Tedhamre <erite601@student.liu.se>
2018-10-10 12:56:02 +0200

    uuuh

--- lab4/search.pl
+++ lab4/search.pl
@@ -2,16 +2,82 @@
 /*
 
 search(
-  [0, [3,3], [0,0], [0,0]]
-  [1, [0,0], [0,0], [3, 3]],
-  P).
+  [0, [1,1], [0,0], [0,0]],
+  [0, [0,0], [1,1], [0,0]]
+).
+
+remove_explored_2(
+  [ [0, [1,1], [0,0], [0,0]], [0, [0,1], [0,0], [0,0]] ],
+  [ [0, [0,0], [1,1], [0,0]], [0, [1,1], [0,0], [0,0]] ],
+  T
+).
 
 */
 
-search(X, Y, P) :-
-  path(X, Y, [], P).
+bf_path([[Leaf|Branch]|Branches], Leaf, [Leaf|Branch]) :-
+  nl, write('1'), nl.
+bf_path([[Leaf|Branch]|Branches], Goal, Path) :-
+  nl, write('2'), nl,
+  children(Leaf, Adjacent),
+  %remove_explored(Adjacent, [[Leaf|Branch]|Branches], Trimmed),
+  remove_explored_2(Adjacent, Branch, Trimmed),
+  dif(Trimmed, []),
+  expand([Leaf|Branch], Trimmed, Expanded),
+  nl, write(Expanded), nl,
+  append(Branches, Expanded, NewBranches),
+  bf_path(NewBranches, Goal, Path).
+bf_path([[Leaf|Branch]|Branches], Goal, Path) :-
+  nl, write('3'), nl,
+  children(Leaf, Leaves),
+  %remove_explored(Leaves, [[Leaf|Branch]|Branches], []),
+  %remove_explored_2(Leaves, Branch, []),
+  bf_path(Branches, Goal, Path).
 
-path(X, Y, V, P).
+remove_explored_2([], _, []).
+remove_explored_2([H1|T1], Branch, [H1|Trimmed]) :-
+  \+exists(H1, Branch),
+  remove_explored_2(T1, Branch, Trimmed).
+remove_explored_2([H1|T1], Branch, Trimmed) :-
+  exists(H1, Branch),
+  remove_explored_2(T1, Branch, Trimmed).
+
+
+remove_explored([], _, []).
+remove_explored([H1|T1], Branches, [H1|Trimmed]) :-
+  \+exists_multi(H1, Branches),
+  remove_explored(T1, Branches, Trimmed).
+remove_explored([H1|T1], Branches, Trimmed) :-
+  exists_multi(H1, Branches),
+  remove_explored(T1, Branches, Trimmed).
+
+exists_multi(E, [BH|BT]) :-
+  exists(E, BH);
+  exists_multi(E, BT).
+
+exists(H, [H|T]).
+exists(E, [H|T]) :-
+  exists(E, T).
+
+search(X, Y) :-
+  bf_path([[X]], Y, P),
+  reverse(P, PP),
+  write_path(PP).
+
+reverse([], []).
+reverse([H|L1], Ln) :-
+  reverse(L1, L2),
+  append(L2, [H], Ln).
+
+
+write_path([]).
+write_path([H|T]) :-
+  write(H), nl,
+  write_path(T).
+
+/* Expand from the course book p.186 */
+expand(X, [], []).
+expand(X, [Y|Z], [[Y|X]|W]) :-
+  expand(X, Z, W).
 
 /* Get all child states of a given state */
 children(S, CC) :-
@@ -33,7 +99,8 @@ ferryable(F) :-
   N > 0.
 
 /* Is a bank survivable? */
-survivable([0,_]).
+survivable([0,C]) :-
+  dif(C, 0).
 survivable([M,C]) :-
   M >= C.
 
@@ -126,10 +193,10 @@ children([0,[3,3],[0,0],[0,0]], C).
 
 children([0,[3,2],[0,1],[0,0]], C).
 [
-  [0,[2,2],[1,1],[0,0]],
-  [0,[3,2],[0,2],[0,0]],
-  [0,[3,3],[0,-1],[0,0]],
-  [1,[3,2],[0,1],[0,0]]
+[0,[2,2],[1,1],[0,0]],
+[0,[3,1],[0,2],[0,0]],
+[0,[3,3],[0,0],[0,0]],
+[1,[3,2],[0,1],[0,0]]
 ]
 
 children([1,[3,2],[0,1],[0,0]], C).
