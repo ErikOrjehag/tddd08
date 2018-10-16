94aa1bb7ef54dcfe485c779d61ac8c938fe2be14
Erik Örjehag <erior950@student.liu.se>
2018-10-16 14:13:28 +0200

    kinda works

--- lab5/schedule.pl
+++ lab5/schedule.pl
@@ -1,59 +1,60 @@
 :- use_module(library(clpfd)).
 
-/*container(0, 2, 2).
-container(1, 4, 1).
-container(2, 2, 2).
-container(3, 1, 1).
-*/
+container(a, 2, 2).
+container(b, 4, 1).
+container(c, 2, 2).
+container(d, 1, 1).
+on(a, d).
+on(b, c).
+on(c, d).
 
-on(4,5).
-
+/*
 container(0, 1, 10).
 container(1, 2, 10).
 container(2, 1, 10).
 container(3, 1, 10).
-/*
+on(4,5).
+
 5*10 = 50 cost <- better
 4* 20 = 80 cost    we want L=5 before L=4
 3* 20 = 60 cost
-[
-start, dur, end, cost, id
-
-T = [
-task(0,2,2,2,0),
-task(0,1,1,4,1),
-task(1,2,3,2,2),
-task(3,1,4,1,3)
-] ?
-
-Kan man räkna ut cost för alla L
-upp till summan av alla resources,
-det högsta antalet som
-går att använda parallellt,
-och sen ta den bästa? !Fullösning!
-
-Det är möjligt att man kan definera resources
-som en funktion av två variabler (Duration and Manpower).
 
 */
 
-start(Tasks, Cost, L) :-
-  findall(ID, container(ID, _, _), Containers),
+answer :-
+  start(Tasks, Cost, Manpower, Duration),
+  write('Manpower: '), write(Manpower), nl,
+  write('Duration: '), write(Duration), nl,
+  write('Cost: '), write(Cost), nl,
+  qsort(Tasks, T),
+  write_schedule(T).
+
+write_schedule([]).
+write_schedule([task(S,D,E,R,ID)|Tasks]) :-
+  write('ID: '), write(ID), write(', '),
+  write('Start: '), write(S), write(', '),
+  write('Duration: '), write(D), write(', '),
+  write('End: '), write(E), write(', '),
+  write('Manpower: '), write(R), write('.'), nl,
+  write_schedule(Tasks).
+
+
+
+start(Tasks, Cost, L, End) :-
+  findall(container(ID, R, D), container(ID, R, D), Containers),
   create_tasks(Containers, Tasks),
   set_constraints(Tasks),
   maplist(task_end, Tasks, Ends),
   maplist(task_start, Tasks, Starts),
-  domain(Ends, 0, 1000),
-  domain(Starts, 0, 1000),
+  domain(Ends, 0, 99999),
+  domain(Starts, 0, 99999),
   maximum(End, Ends),
-  L in 0..100,
-  indomain(L),
+  L in 0..99999,
   cumulative(Tasks, [limit(L)]),
   Cost #= L * End,
   append(Starts, Ends, V0),
-  append(V0, [Cost, L, End], Vars),
+  append(V0, [Cost], Vars),
   labeling([minimize(Cost)], Vars).
-
 
 task_end(task(_,_,End,_,_), End).
 
@@ -65,8 +66,7 @@ maplist( C_2, [X|Xs], [Y|Ys]) :-
    maplist( C_2, Xs, Ys).
 
 create_tasks([], []).
-create_tasks([ID|T], [task(_,D,_,R,ID)|Tasks]) :-
-  container(ID, R, D),
+create_tasks([container(ID, R, D)|T], [task(_,D,_,R,ID)|Tasks]) :-
   create_tasks(T, Tasks).
 
 set_constraints([]).
@@ -78,15 +78,42 @@ set_constraints_help(_, _, _, []).
 set_constraints_help(ID, S, E, [task(Si,_,Ei,_,IDi)|Tasks]) :-
   on(ID, IDi),
   Si #>= E,
-  write(ID), write('>='), write(IDi), nl,
   set_constraints_help(ID, S, E, Tasks).
 set_constraints_help(ID, S, E, [task(Si,_,Ei,_,IDi)|Tasks]) :-
   on(IDi, ID),
   S #>= Ei,
-  write(IDi), write('>='), write(ID), nl,
   set_constraints_help(ID, S, E, Tasks).
 set_constraints_help(ID, S, E, [task(Si,_,Ei,_,IDi)|Tasks]) :-
   \+on(ID, IDi),
   \+on(IDi, ID),
-  write(ID), write(' - '), write(IDi), nl,
   set_constraints_help(ID, S, E, Tasks).
+
+
+
+
+  /*
+  Partitions a list into into one list
+  containing all the elements bigger than the
+  pivot element and one list with all the smaller elements.
+  */
+  partition(_, [], [], []).
+  partition(Pivot, [Elem|T], S, [Elem|G]) :-
+    Pivot = task(X,_,_,_,_),
+    Elem = task(H,_,_,_,_),
+    (H >= X),
+    partition(Pivot, T, S, G).
+  partition(Pivot, [Elem|T], [Elem|S], G) :-
+    Pivot = task(X,_,_,_,_),
+    Elem = task(H,_,_,_,_),
+    (H <  X),
+    partition(Pivot, T, S, G).
+
+  /*
+  Quicksort using the partition predicate
+  */
+  qsort([], []).
+  qsort([X|T], L1) :-
+    partition(X, T, S, G),
+    qsort(S, S1),
+    qsort(G, G1),
+    append(S1, [X|G1], L1).
