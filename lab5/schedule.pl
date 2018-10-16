e9d80dbd49a93e7a9982a4e8a2d390793a1fa37d
Erik Örjehag <erior950@student.liu.se>
2018-10-16 15:00:37 +0200

    whey

--- lab5/schedule.pl
+++ lab5/schedule.pl
@@ -1,48 +1,27 @@
 :- use_module(library(clpfd)).
 
-container(a, 2, 2).
-container(b, 4, 1).
-container(c, 2, 2).
-container(d, 1, 1).
-on(a, d).
-on(b, c).
-on(c, d).
-
-/*
-container(0, 1, 10).
-container(1, 2, 10).
-container(2, 1, 10).
-container(3, 1, 10).
-on(4,5).
-
-5*10 = 50 cost <- better
-4* 20 = 80 cost    we want L=5 before L=4
-3* 20 = 60 cost
-
-*/
-
 answer :-
   start(Tasks, Cost, Manpower, Duration),
   write('Manpower: '), write(Manpower), nl,
   write('Duration: '), write(Duration), nl,
   write('Cost: '), write(Cost), nl,
   qsort(Tasks, T),
-  write_schedule(T).
+  write('Schedule as follows:'), nl,
+  format("|~`-t~31||~n", []),
+  format("|~t~a~3||~t~a~6+|~t~a~9+|~t~a~4+|~t~a~9+|~n", ['ID','Start','Duration','End','Manpower']),
+  write_schedule(T),
+  format("|~`-t~31||~n", []),
+  retractall(id(_,_)).
 
 write_schedule([]).
-write_schedule([task(S,D,E,R,ID)|Tasks]) :-
-  write('ID: '), write(ID), write(', '),
-  write('Start: '), write(S), write(', '),
-  write('Duration: '), write(D), write(', '),
-  write('End: '), write(E), write(', '),
-  write('Manpower: '), write(R), write('.'), nl,
+write_schedule([task(S,D,E,R,INT_ID)|Tasks]) :-
+  id(ID, INT_ID),
+  format("|~t~a~3||~t~d~6+|~t~d~9+|~t~d~4+|~t~d~9+|~n", [ID,S,D,E,R]),
   write_schedule(Tasks).
-
-
 
 start(Tasks, Cost, L, End) :-
   findall(container(ID, R, D), container(ID, R, D), Containers),
-  create_tasks(Containers, Tasks),
+  create_tasks(0, Containers, Tasks),
   set_constraints(Tasks),
   maplist(task_end, Tasks, Ends),
   maplist(task_start, Tasks, Starts),
@@ -65,55 +44,120 @@ maplist( C_2, [X|Xs], [Y|Ys]) :-
    call(C_2, X, Y),
    maplist( C_2, Xs, Ys).
 
-create_tasks([], []).
-create_tasks([container(ID, R, D)|T], [task(_,D,_,R,ID)|Tasks]) :-
-  create_tasks(T, Tasks).
+create_tasks(_, [], []).
+create_tasks(INT, [container(ID, R, D)|T], [task(_,D,_,R,INT)|Tasks]) :-
+  assert(id(ID, INT)),
+  I is INT + 1,
+  create_tasks(I, T, Tasks).
 
+/*
+
+*/
 set_constraints([]).
 set_constraints([task(S,D,E,R,ID)|Tasks]) :-
   set_constraints_help(ID, S, E, Tasks),
   set_constraints(Tasks).
 
 set_constraints_help(_, _, _, []).
-set_constraints_help(ID, S, E, [task(Si,_,Ei,_,IDi)|Tasks]) :-
+set_constraints_help(INT_ID, S, E, [task(Si,_,Ei,_,INT_IDi)|Tasks]) :-
+  id(ID, INT_ID),
+  id(IDi, INT_IDi),
   on(ID, IDi),
   Si #>= E,
-  set_constraints_help(ID, S, E, Tasks).
-set_constraints_help(ID, S, E, [task(Si,_,Ei,_,IDi)|Tasks]) :-
+  set_constraints_help(INT_ID, S, E, Tasks).
+set_constraints_help(INT_ID, S, E, [task(Si,_,Ei,_,INT_IDi)|Tasks]) :-
+  id(ID, INT_ID),
+  id(IDi, INT_IDi),
   on(IDi, ID),
   S #>= Ei,
-  set_constraints_help(ID, S, E, Tasks).
-set_constraints_help(ID, S, E, [task(Si,_,Ei,_,IDi)|Tasks]) :-
+  set_constraints_help(INT_ID, S, E, Tasks).
+set_constraints_help(INT_ID, S, E, [task(Si,_,Ei,_,INT_IDi)|Tasks]) :-
+  id(ID, INT_ID),
+  id(IDi, INT_IDi),
   \+on(ID, IDi),
   \+on(IDi, ID),
-  set_constraints_help(ID, S, E, Tasks).
+  set_constraints_help(INT_ID, S, E, Tasks).
+
+/*
+Partitions a list of tasks into one list
+containing all the elements bigger than the
+pivot element and one list with all the smaller elements.
+*/
+partition(_, [], [], []).
+partition(Pivot, [Elem|T], S, [Elem|G]) :-
+  Pivot = task(X,_,_,_,_),
+  Elem = task(H,_,_,_,_),
+  (H >= X),
+  partition(Pivot, T, S, G).
+partition(Pivot, [Elem|T], [Elem|S], G) :-
+  Pivot = task(X,_,_,_,_),
+  Elem = task(H,_,_,_,_),
+  (H <  X),
+  partition(Pivot, T, S, G).
+
+/*
+Quicksort of tasks using the partition predicate
+*/
+qsort([], []).
+qsort([X|T], L1) :-
+  partition(X, T, S, G),
+  qsort(S, S1),
+  qsort(G, G1),
+  append(S1, [X|G1], L1).
+
+/*
+EXAMPLE 1:
+
+container(a, 2, 2).
+container(b, 4, 1).
+container(c, 2, 2).
+container(d, 1, 1).
+on(a, d).
+on(b, c).
+on(c, d).
+
+| ?- answer.
+Manpower: 4
+Duration: 4
+Cost: 16
+Schedule as follows:
+|------------------------------|
+|ID|Start|Duration|End|Manpower|
+| b|    0|       1|  1|       4|
+| a|    1|       2|  3|       2|
+| c|    1|       2|  3|       2|
+| d|    3|       1|  4|       1|
+|------------------------------|
+yes
 
 
+EXAMPLE 2:
+The purpose of this example is to check that the minimum cost
+is selected and not the minimum number of people hired.
 
+container(a, 1, 10).
+container(b, 2, 10).
+container(c, 1, 10).
+container(d, 1, 10).
+on(e,f).
 
-  /*
-  Partitions a list into into one list
-  containing all the elements bigger than the
-  pivot element and one list with all the smaller elements.
-  */
-  partition(_, [], [], []).
-  partition(Pivot, [Elem|T], S, [Elem|G]) :-
-    Pivot = task(X,_,_,_,_),
-    Elem = task(H,_,_,_,_),
-    (H >= X),
-    partition(Pivot, T, S, G).
-  partition(Pivot, [Elem|T], [Elem|S], G) :-
-    Pivot = task(X,_,_,_,_),
-    Elem = task(H,_,_,_,_),
-    (H <  X),
-    partition(Pivot, T, S, G).
+Manpower * Duration = Cost:
+5 * 10 = 50 cost    <- The best one even though 5 > 3
+4 * 20 = 80 cost
+3 * 20 = 60 cost
 
-  /*
-  Quicksort using the partition predicate
-  */
-  qsort([], []).
-  qsort([X|T], L1) :-
-    partition(X, T, S, G),
-    qsort(S, S1),
-    qsort(G, G1),
-    append(S1, [X|G1], L1).
+| ?- answer.
+Manpower: 5
+Duration: 10
+Cost: 50
+Schedule as follows:
+|------------------------------|
+|ID|Start|Duration|End|Manpower|
+| a|    0|      10| 10|       1|
+| b|    0|      10| 10|       2|
+| c|    0|      10| 10|       1|
+| d|    0|      10| 10|       1|
+|------------------------------|
+yes
+
+*/
